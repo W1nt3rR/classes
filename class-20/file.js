d25bc29716f3337aa576d7780fb04091c0542306
W1nt3rR <frostywinterr@gmail.com>
2022-04-07 17:54:27 +0200

    more js

---
+++ class-20/file.js
@@ -0,0 +1,41 @@
+// var broj = true && true;
+// //broj.toString();
+// console.log(typeof broj.toString());
+// console.log(typeof(broj));
+// console.log(typeof broj);
+// console.log(broj);
+
+
+/*
+var a = 10;
+var b = 20;
+var temp = a;
+
+console.log(a,b);
+
+a = b;
+b = temp;
+
+
+console.log(a,b);
+*/
+
+/*
+var temp = "WinterR";
+temp = temp.toLowerCase();
+
+console.log(temp[0]);
+*/
+
+/*
+var age = 21;
+var fullGreetings = `I am ${age} years old`;
+var greetings = "I am " + age + " years old";
+
+console.log(fullGreetings);
+console.log(greetings);
+*/
+
+var a = (false && true) || ("false" || "true")
+
+console.log(a);
