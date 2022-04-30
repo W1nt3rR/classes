123bb42e0c2d4ce3af48d8969f87c74c92820765
W1nt3rR <frostywinterr@gmail.com>
2022-04-30 18:02:25 +0200

    classes

---
+++ class-29/javascript.js
@@ -0,0 +1,62 @@
+/*
+
+const obj = 
+{
+    user: "Ramiz",
+    age: 21,
+    adress: 
+    {
+        street: "Hotkovo",
+        number: 69,
+        city: "Novi Pazar"
+    }
+}
+
+//destructuring
+const { user, age, adress } = obj;
+const {street, number, city} = adress;
+
+console.log(street);
+
+*/
+
+/*
+
+const [a, b, c, d] = [1, 2, 3, 4, 5];
+const [x, , , , y] = [1, 2, 3, 4, 5];
+
+console.log(a);
+console.log(y);
+
+*/
+
+/*
+
+class Car 
+{
+    constructor(type, price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    getPrice() {
+        return this.price;
+    }
+
+    getType() {
+        return this.type;
+    }
+
+    toString() {
+        return JSON.stringify(this);
+    }
+}
+
+const car1 = new Car("Tesla", 100000);
+const car2 = new Car("BMW", 100000);
+
+console.log(car1.toString());
+
+console.log(car2);
+
+*/
